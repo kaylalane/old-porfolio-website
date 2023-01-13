cc7af9ef85fe04b3b94a47df1906288bdf3d4376
kaylalane <73676075+kaylalane@users.noreply.github.com>
2023-01-12 20:23:08 -0500

    Fix nav

---
+++ src/script.js
@@ -0,0 +1,38 @@
+const { doc } = require("prettier");
+
+function pageNavigation(evt, navLink) {
+
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabContent");
+  tablinks = document.getElementsByClassName("tabLinks");
+
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(navLink).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+function responsiveMenu() {
+  var x = document.getElementById("nav");
+  if (x.className === "navBar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navBar";
+  }
+}
+
+let menu = document.querySelector('#menu-icon');
+let navBar = document.querySelector('.navBar');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navBar.classList.toggle('open');
+}
